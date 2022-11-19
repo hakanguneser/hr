084a60155e43bad629dd05aa7522577e33d1c3a4
hakanguneser <hakan.guneser@gmail.com>
2022-11-19 20:39:59 +0300

    Department Repository created
    Repository and its unit tests created

---
+++ src/main/java/com/avinty/hr/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.avinty.hr.repository;
+
+import com.avinty.hr.model.entity.DepartmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Integer> {
+}
