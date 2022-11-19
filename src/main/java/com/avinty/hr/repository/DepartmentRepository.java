327bef604957a255c125d347d7070528fe73257f
hakanguneser <hakan.guneser@gmail.com>
2022-11-19 20:47:44 +0300

    Revert "Department Repository created"
    This reverts commit 084a60155e43bad629dd05aa7522577e33d1c3a4.

--- src/main/java/com/avinty/hr/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avinty.hr.repository;
-
-import com.avinty.hr.model.entity.DepartmentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Integer> {
-}
